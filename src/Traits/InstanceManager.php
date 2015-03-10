54a3cc382d7f1984e7f95964393f292297357128
mikakane <mikakane2@gmail.com>
2015-03-10 12:47:31 +0900

    update

---
+++ src/Traits/InstanceManager.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mkkn
+ * Date: 2015/02/28
+ * Time: 20:15
+ */
+namespace Chatbox\Traits;
+
+use Chatbox\Arr;
+
+trait InstanceManager {
+
+    static private $instances = [];
+
+    /**
+     * @param $obj
+     * @param string $name
+     */
+    static public function setInstance($obj,$name="active"){
+        static::$instances[$name] = $obj;
+    }
+
+    /**
+     * @param string $name
+     * @return static
+     * @throws \DomainException
+     */
+    static public function getInstance($name="active"){
+        if(isset(static::$instances[$name])){
+            return static::$instances[$name];
+        }else{
+            throw new \DomainException("");
+        }
+    }
+}
